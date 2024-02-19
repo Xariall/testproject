9b6742d47f49c8a6ef0c551a3ab3392ce2573f60
xariell <supersanin45@gmail.com>
2024-02-19 10:36:28 +0600

    first commit

---
+++ src/test/java/com/salam/olxproject/OlxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salam.olxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlxProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
